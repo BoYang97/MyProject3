5427fe4c2cd644eebac0df5579bc486d057bc01d
BoYang97 <50433721+BoYang97@users.noreply.github.com>
2019-11-10 02:07:44 -0800

    first rough prototype

---
+++ Source/MyProject3/PaperMan_GameMode.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaperMan_GameMode.h"
+
---
+++ Source/MyProject3/PaperMan_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PaperMan_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT3_API APaperMan_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
